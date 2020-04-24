46cfb1ad84af1da0083373db6f0ac43783543592
Diego-sdd <diegodiasantana@gmail.com>
2020-04-24 01:45:39 -0300

    add

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import Index from './pages';
+import Index from './pages/index.js';
 
 export default function Routes() {
     return (
