f0061a12d009dddb684b35a1e3ed52f98df8ded2
Diego-sdd <diegodiasantana@gmail.com>
2020-04-27 08:20:54 -0300

    add

--- src/pages/graphic/Donut/index.js
+++ src/pages/graphic/Donut/index.js
@@ -5,23 +5,8 @@ import { BsFillCircleFill } from 'react-icons/bs';
 import icon_hat from '../../assets/university.png';
 import './style.css';
 import './responsive_style.css';
-//import data from '../../../arquivo.json';
 export default function Graphic_Donut() {
-    // const student = data.student;
-    // useEffect(() => {
-    //     const qt_grupe = student.map(students => (
-    //         students.ds_groups
-    //     ));
-    //     var indices = [];
-    //     var idx = qt_grupe.indexOf('');
-    //     while (idx != -1) {
-    //         indices.push(idx);
-    //         idx = qt_grupe.indexOf(3, idx + 1);
-    //     }
 
-    //     console.log(indices)
-
-    // })
     return (
         <Card className="card_chart_pie">
             <div id="top_text">
@@ -43,16 +28,7 @@ export default function Graphic_Donut() {
                     ['aaaaa', 2],
                 ]}
                 options={{
-                    //title: 'My Daily Activities',
-                    // Just add this option
                     legend: 'none',
-                    // legend: {
-                    //     textStyle: { fontSize: 12, color: 'red' },
-                    //     position: 'bottom',
-                    //     alignment: 'start',
-                    //     maxLines: 1
-                    // },
-
                     pieHole: 0.8,
                     pieSliceText: 'none',
                     colors: ['#FF4C61', '#33D69F', '#6F52ED', '#FFB800']
@@ -65,8 +41,6 @@ export default function Graphic_Donut() {
                     <p id="text_icon">&nbsp;&nbsp;Groups of 20 students </p>
                 </div>
                 <p id="text_number"><strong>20&nbsp;</strong>(32%)</p>
-
-
                 <div id="text_icon">
                     <BsFillCircleFill id="icon_ball_two" />
                     <p id="text_icon">&nbsp;&nbsp;Groups of 10 students </p>
